23558e06b9a19e1d43d77271cbdbd136c7bd77fe
“Satoshi” <sprint1091.to@gmail.com>
2019-03-14 00:15:17 +0000

    User.rbの不要な空行の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
   enum gender: {man: 1, woman: 2}
 end
