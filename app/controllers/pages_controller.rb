64b125ff7d15ad1778a99d373d05d517603685ce
“Satoshi” <sprint1091.to@gmail.com>
2019-03-15 01:21:26 +0000

    オーバーライドしたDeviseコントローラの削除,それに伴うroutesの変更,pagesコントローラで代替

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,45 @@
 class PagesController < ApplicationController
+  
   def index
+    if user_signed_in?
+      @users = User.all
+    else
+      render 'top_page'
+    end
   end
-
+  
   def show
   end
+  
+  def profile_edit
+  end
+  
+  def profile_update
+    current_user.assign_attributes(account_update_params)
+    if current_user.save
+	    redirect_to action: :index
+    else
+      render "profile_edit"
+    end
+  end
+  
+  
+  private
+    def sign_up_params
+      params.require(:user).permit(
+        :email,
+        :password,
+        :password_confirmation)
+    end
+    
+    def account_update_params
+      params.require(:user).permit(
+        :username,
+        :birthdate,
+        :gender,
+        :school,
+        :email,
+        :password,
+        :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-  :sessions      => "users/sessions",
-  :registrations => "users/registrations",
-  :passwords     => "users/passwords"
-} 
+  devise_for :users
   devise_scope :user do
-    get 'users/:id/edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
-    get 'index', to: 'users/registrations#index', as: 'index'
-    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
-    root :to => "users/registrations#index"
+    get 'users/:id/edit', to: 'pages#profile_edit', as: 'profile_edit'
+    get 'index', to: 'pages#index', as: 'index'
+    patch 'profile_update', to: 'pages#profile_update', as: 'profile_update'
+    root :to => "pages#index"
   end
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
