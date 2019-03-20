c9193d5bed836fe937b97833a3eb0fff6ab508dc
“Satoshi” <sprint1091.to@gmail.com>
2019-03-20 10:00:26 +0000

    devise_scopeの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#index', as: 'index'
   devise_for :users
-  devise_scope :user do
-    get 'users/:id/edit', to: 'pages#profile_edit', as: 'profile_edit'
-    get 'index', to: 'pages#index', as: 'index'
-    patch 'profile_update', to: 'pages#profile_update', as: 'profile_update'
-    root :to => "pages#index"
-  end
-  get 'pages/show'
+  get 'users/:id/edit', to: 'pages#profile_edit', as: 'profile_edit'
+  patch 'profile_update', to: 'pages#profile_update', as: 'profile_update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
