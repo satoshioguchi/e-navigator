e0b0211431b32d71ea07b0964b78ffc94377908f
“Satoshi” <sprint1091.to@gmail.com>
2019-03-25 17:27:02 +0000

    routesにusersリソースの追加、pagesリソースの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index', as: 'index'
   devise_for :users
-  get 'users/:id/edit', to: 'pages#profile_edit', as: 'profile_edit'
-  patch 'profile_update', to: 'pages#profile_update', as: 'profile_update'
+  resources  :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
