b2b4f783c742791507f2c29f47f77246cdfa90de
“Satoshi” <sprint1091.to@gmail.com>
2019-03-25 17:39:28 +0000

    rootをusers#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#index', as: 'index'
+  root 'users#index', as: 'index'
   devise_for :users
   resources  :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
