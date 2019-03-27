04a6d34037ffc5cead966de612a5a504b576ba47
“Satoshi” <sprint1091.to@gmail.com>
2019-03-27 10:39:56 +0000

    routesに/users/:id/interviewsを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index', as: 'index'
   devise_for :users
-  resources  :users, only: [:index, :edit, :update]
+  resources  :users, only: [:index, :edit, :update] do
+    member do
+      get :interviews
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
