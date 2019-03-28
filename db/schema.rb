e866c02e5272445aabf3f2021c358b17c76df4c2
“Satoshi” <sprint1091.to@gmail.com>
2019-03-28 19:18:43 +0000

    rails db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190228132656) do
+ActiveRecord::Schema.define(version: 20190328191310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.time "schedule"
+    t.integer "propriety"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
