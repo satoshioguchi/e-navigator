66cf276df7d412d2828db2952b58379edc6bf465
“Satoshi” <sprint1091.to@gmail.com>
2019-03-30 15:16:51 +0000

    routesにinterviewsリソースを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'interviews/index'
-
-  get 'interviews/new'
-
-  get 'interviews/edit'
-
   root 'users#index', as: 'index'
   devise_for :users
   resources  :users, only: [:index, :edit, :update] do
     member do
-      get :interviews
+      get :interviews, to: 'interviews#index'
     end
   end
+  resources :interviews, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
