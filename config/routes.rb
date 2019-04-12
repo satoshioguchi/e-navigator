c3070e7be5a868f862a06b105fdd6c2c2b0dbb34
“Satoshi” <sprint1091.to@gmail.com>
2019-04-12 17:31:28 +0000

    UserリソースとInterviewリソースをネストする書き方を修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root 'users#index', as: 'index'
   devise_for :users
   resources  :users, only: [:index, :edit, :update] do
-    member do
-      resources :interviews, except: [:show]
-    end
+    resources :interviews
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
