75039a57a6f7808de0d3f05b492f4a9f5a22b1ca
“Satoshi” <sprint1091.to@gmail.com>
2019-04-12 17:29:57 +0000

    interviewsコントローラのCRUD操作のアクションを完成させた

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,7 +1,12 @@
 class InterviewsController < ApplicationController
+  
   def index
-    @user = User.find(params[:id])
-    @interviews = Interview.where(user_id: params[:id])
+    @user = User.find(params[:user_id])
+    @interviews = Interview.where(user_id: params[:user_id])
+  end
+  
+  def show
+    @interview = Interview.find_by(user_id: params[:user_id], id: params[:id])
   end
 
   def new
@@ -12,19 +17,31 @@ class InterviewsController < ApplicationController
     @interview = current_user.interviews.build(interview_params)
     @interview.propriety = 3
     if @interview.save
-      redirect_to interviews_path
+      redirect_to user_interviews_path
     else
       render :new
     end
   end
 
   def edit
+    @interview = Interview.find_by(user_id: params[:user_id], id: params[:id])
   end
   
   def update
+    @interview = Interview.find_by(user_id: current_user.id)
+    if @interview.update_attributes(interview_params)
+      flash[:success] = "面接が更新されました"
+      redirect_to controller: 'interviews', action: 'show'
+    else
+      render edit
+    end
   end
   
   def destroy
+    @interview = Interview.find_by(user_id: params[:user_id], id: params[:id])
+    @interview.destroy
+    flash[:success] = "面接が削除されました"
+    redirect_to controller: 'interviews', action: 'index'
   end
   
   private
