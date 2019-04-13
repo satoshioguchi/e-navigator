d5fc1af77b0514c132495a3240ad92bb5b55cf6d
“Satoshi” <sprint1091.to@gmail.com>
2019-04-13 20:07:55 +0000

    routesにpatchリクエストの/users/:user_id/interviews/:interview_id/approveを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'users#index', as: 'index'
   devise_for :users
   resources  :users, only: [:index, :edit, :update] do
-    resources :interviews
+    resources :interviews do
+      patch :approve
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
