8c64c1562360f5e5a275fb483287cab7c5d5ef4b
“Satoshi” <sprint1091.to@gmail.com>
2019-04-15 10:35:22 +0000

    notification_mailer.rbにinterview_decisionメソッドを追加

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,13 @@
 class NotificationMailer < ApplicationMailer
+  
+  def interview_decision(user, interviewer, interview)
+    @user = user
+    @interviewer = interviewer
+    @interview = interview
+    mail(
+    from:    'test@example.com',
+    to:       "#{@user.email}, #{@interviewer.email}",
+    subject: '面接日時が確定しました'
+    )
+  end
 end
