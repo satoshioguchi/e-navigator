418f45c8e4ec6ab04c16b6ff6b8333c1aad931e5
“Satoshi” <sprint1091.to@gmail.com>
2019-04-17 07:29:54 +0000

    Userモデルにattr_accessor :request_userを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :request_user
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum gender: {man: 1, woman: 2}
