193eba4783af4c3368d59eed8191fc3356144c95
“Satoshi” <sprint1091.to@gmail.com>
2019-04-25 18:27:12 +0000

    user.rbの attr_accessor :request_userを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  attr_accessor :request_user
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum gender: {man: 1, woman: 2}
