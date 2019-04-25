5089b9922e182821a7b71b56bb125dfb74207146
“Satoshi” <sprint1091.to@gmail.com>
2019-04-25 16:24:28 +0000

    Interview.rbのenumを英語表記に修正

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,5 +1,5 @@
 class Interview < ApplicationRecord
   default_scope -> { order(schedule: :asc) }
   belongs_to :user
-   enum propriety: {却下: 1, 承認: 2, 保留: 3}
+   enum propriety: {reject: 1, approval: 2, reservation: 3}
 end
