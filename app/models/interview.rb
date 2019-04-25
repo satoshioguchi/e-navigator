c6ff43af1f0005b3b239f2954b7a36c42016439a
“Satoshi” <sprint1091.to@gmail.com>
2019-04-25 17:10:49 +0000

    interview.rbの不要な空白を削除

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,5 +1,5 @@
 class Interview < ApplicationRecord
   default_scope -> { order(schedule: :asc) }
   belongs_to :user
-   enum propriety: {reject: 1, approval: 2, reservation: 3}
+  enum propriety: {reject: 1, approval: 2, reservation: 3}
 end
